0dbdaa32e5dc6ccc1780e7e94c3712c6abb3fae3
Julian McNeill <julesdowork@gmail.com>
2018-05-09 13:25:24 -0400

    Functions with Variable Parameters

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -4,22 +4,20 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour {
 
-    private string name = "Jules";
-
-	// Use this for initialization
+    // Use this for initialization
 	void Start () {
-        ShowMainMenu();
+        string name = "Jules";
+        ShowMainMenu(name);
     }
 
-    void ShowMainMenu () {
+    void ShowMainMenu (string aName) {
         Terminal.ClearScreen();
-        string greeting = "Hello " + name;
-        Terminal.WriteLine(greeting);
+        Terminal.WriteLine("Hello " + aName);
         Terminal.WriteLine("Choose a target to hack into:\n");
         Terminal.WriteLine("Press 1 for Server County Library");
         Terminal.WriteLine("Press 2 for Dells Largo Bank");
         Terminal.WriteLine("Press 3 for the NSA\n");
-        Terminal.WriteLine("HS C:\\Users\\" + name + ">");
+        Terminal.WriteLine("HS C:\\Users\\" + aName + ">");
     }
 	
 	// Update is called once per frame
