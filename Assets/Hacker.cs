cd5151184663972badd861468a6cfe671e0928ab
Julian McNeill <julesdowork@gmail.com>
2018-05-09 13:17:40 -0400

    Introducing Variables

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour {
 
+    private string name = "Jules";
+
 	// Use this for initialization
 	void Start () {
         ShowMainMenu();
@@ -11,12 +13,13 @@ public class Hacker : MonoBehaviour {
 
     void ShowMainMenu () {
         Terminal.ClearScreen();
-        Terminal.WriteLine("Linucks HackerShell\n");
+        string greeting = "Hello " + name;
+        Terminal.WriteLine(greeting);
         Terminal.WriteLine("Choose a target to hack into:\n");
         Terminal.WriteLine("Press 1 for Server County Library");
         Terminal.WriteLine("Press 2 for Dells Largo Bank");
         Terminal.WriteLine("Press 3 for the NSA\n");
-        Terminal.WriteLine("HS C:\\Users\\Agent001>");
+        Terminal.WriteLine("HS C:\\Users\\" + name + ">");
     }
 	
 	// Update is called once per frame
