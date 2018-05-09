57627b65794533a19878ed3c32a889bf72a2e58b
Julian McNeill <julesdowork@gmail.com>
2018-05-08 20:34:41 -0400

    Using our Terminal.WriteLine()

---
+++ Assets/Hacker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //print("Hello Console");
+        //Terminal.WriteLine("Hello Terminal");
+
+        Terminal.WriteLine("Linucks HackerShell\n");
+        Terminal.WriteLine("Choose a target to hack into:\n");
+        Terminal.WriteLine("Press 1 for Server County Library");
+        Terminal.WriteLine("Press 2 for Dells Largo Bank");
+        Terminal.WriteLine("Press 3 for the NSA\n");
+        Terminal.WriteLine("HS C:\\Users\\Agent001>");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
